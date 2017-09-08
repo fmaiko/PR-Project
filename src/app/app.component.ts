c9a4b6c83146792ba21b705ac1f567eea0190fe2
maikofelix89 <maikofelix89@gmail.com>
2017-09-08 10:59:15 +0300

    Added second Line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'You know nothing John Snow';
+  title = 'You know nothing John Snow! King of the North';
 }
